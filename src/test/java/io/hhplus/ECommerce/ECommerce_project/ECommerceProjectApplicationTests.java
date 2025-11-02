006de5e513b05142079f3dfb0c36bbd81e13f0f0
parkboseung <endeveloper96@gmail.com>
2025-11-02 17:26:35 +0900

    first commit

---
+++ src/test/java/io/hhplus/ECommerce/ECommerce_project/ECommerceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hhplus.ECommerce.ECommerce_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
